142c40c761dfd4b39edc70e47a957d9d0d467c5e
Sumanth Hegde <sumanthhegde002@gmail.com>
2026-03-06 18:49:22 +0530

    Refactor EmployeesDriver for employee details input
    Refactored EmployeesDriver to improve employee data input and display using threads.

--- EmployeesDriver.java
+++ EmployeesDriver.java
@@ -1,44 +1,113 @@
 package prg5;
+
 import java.util.Scanner;
 
+interface PersonalDetails{
+    void showPersonalDetails();
+}
+
+interface OfficialDetails{
+    void showOfficialDetails();
+}
+
+class Employee implements PersonalDetails, OfficialDetails{
+
+    String name, designation, department;
+    int age, uid, empid;
+
+    Employee(int uid, String name, int age, int empid, String designation, String department){
+        this.uid = uid;
+        this.name = name;
+        this.age = age;
+        this.empid = empid;
+        this.designation = designation;
+        this.department = department;
+    }
+
+    @Override
+    public void showPersonalDetails(){
+        System.out.println("\nEmployee Personal Details");
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("UID : " + uid);
+    }
+
+    @Override
+    public void showOfficialDetails(){
+        System.out.println("\nEmployee Official Details");
+        System.out.println("Employee ID : " + empid);
+        System.out.println("Designation : " + designation);
+        System.out.println("Department : " + department);
+    }
+}
+
+class PersonalThread extends Thread{
+    Employee emp;
+
+    PersonalThread(Employee emp){
+        this.emp = emp;
+    }
+
+    public void run(){
+        emp.showPersonalDetails();
+    }
+}
+
+class OfficialThread extends Thread{
+    Employee emp;
+
+    OfficialThread(Employee emp){
+        this.emp = emp;
+    }
+
+    public void run(){
+        emp.showOfficialDetails();
+    }
+}
+
 public class EmployeesDriver {
-//	static int count=0; 
-//	public static Employee getDetails(Employee e, Scanner scan) {
-//		System.out.println("Enter the details for Employee "+count+" : ");
-//		String name, designation, department;
-//		int age, uid, empid;
-//		System.out.println("Enter Name, Age :");
-//		name = scan.nextLine();
-//		age = scan.nextInt();
-//		uid = count;
-//		System.out.println("Enter Employee Designation, Department : ");
-//		designation = scan.next();
-//		department = scan.nextLine();
-//		empid = 100 + count;
-//		e = new Employee(uid,name,age, empid, designation, department);
-//		count++;
-//		return e;
-//	}
 
-	public static void main(String[] args) {
-		Employee[] emp = new Employee[3] ;
-		Scanner scan = new Scanner(System.in);
-		for(int i=1; i<3;i++) {
-			System.out.println("Enter the details for Employee "+i+" : ");
-			String name, designation, department;
-			int age, uid, empid;
-			System.out.println("Enter Name, Age , UID:");
-			name = scan.nextLine();
-			age = scan.nextInt();
-			uid = scan.nextInt();
-			System.out.println("Enter Employee Designation, Department, Empid : ");
-			designation = scan.next();
-			department = scan.nextLine();
-			empid = scan.nextInt();
-			emp[i] = new Employee(uid,name,age, empid, designation, department);
-		}
-		scan.close();
-		emp[0].showOfficialDetails();
-	}
+    public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        Employee[] emp = new Employee[3];
+
+        for(int i = 0; i < 3; i++){
+
+            System.out.println("\nEnter details for Employee " + (i+1));
+
+            System.out.println("Enter Name:");
+            String name = scan.next();
+
+            System.out.println("Enter Age:");
+            int age = scan.nextInt();
+
+            System.out.println("Enter UID:");
+            int uid = scan.nextInt();
+
+            System.out.println("Enter Employee ID:");
+            int empid = scan.nextInt();
+
+            System.out.println("Enter Designation:");
+            String designation = scan.next();
+
+            System.out.println("Enter Department:");
+            String department = scan.next();
+
+            emp[i] = new Employee(uid, name, age, empid, designation, department);
+        }
+
+        // Creating threads
+        for(int i = 0; i < 3; i++){
+
+            PersonalThread t1 = new PersonalThread(emp[i]);
+            OfficialThread t2 = new OfficialThread(emp[i]);
+
+            t1.start();
+            t2.start();
+        }
+
+        scan.close();
+    }
 }
